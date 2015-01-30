5dda41057621a96b1b2cdf6173c7301a610e1ed0
TREY <rkh.mymail@gmail.com>
2015-01-30 04:07:20 -0500

    watched stocks not updating with new price data for some reason

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -12,14 +12,15 @@ class StocksController < ApplicationController
   end
 
   def create
-  	new_stock = current_user.stocks.new(stock_params)
+  	new_stock = current_user.stocks.find_or_initialize_by(symbol: params[:symbol])
+    new_stock.price = params[:price]
   	if new_stock.save
   		render json: current_user.stocks
   	end
   end
 
   def destroy
-  	new_stock = current_user.stocks.new(stock_params)
+  	new_stock = current_user.stocks.new(params)
   	if new_stock.destroy
   		render json: current_user.stocks
   	end
